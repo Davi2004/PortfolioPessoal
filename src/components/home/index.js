f8e9e0322baceb2565edec202b7b673a61955bda
Davimrp <davipereirar39@gmail.com>
2025-10-15 12:27:36 -0300

    Atualizando página de Contatos

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -168,20 +168,29 @@ function Home() {
 
                 <div className='containerContatos'>
 
-
-                    <div className='colors one'>
-                        <PhoneCall size={50} color='#08082F'/>
+                    <div className='colors right'>
+                        <PhoneCall size={30} color='#08082F'/>
                         <p> +55 (77) 99171-8244 </p>
                     </div>
                     
-                    <div className='colors two'>
-                        <Mail size={50} color='#08082F'/>
+                    <div className='colors left'>
+                        <Mail size={30} color='#08082F'/>
                         <a href='mailto:davipereirar39@gmail.com'> davipereirar39@gmail.com </a>
+                    </div>
+                    
+                    <div className='colors right'>
+                        <Linkedin size={25} color='#08082F'/>
+                        <a href='https://www.linkedin.com/in/davipereira-dev' target='_blank' rel='noreferrer'> https://www.linkedin.com/in/davipereira-dev </a>
+                    </div>
+                    
+                    <div className='colors left'>
+                        <Github size={30} color='#08082F'/>
+                        <a href='https://github.com/Davi2004' target='_blank' rel='noreferrer'> https://github.com/Davi2004 </a>
                     </div>
                                         
                 </div>
                 
-            </section>            
+            </section>   
             
         </main>
 
